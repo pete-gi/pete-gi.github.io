2ce3196dc29c3138f82b9369a91c79016b3a81a5
pete-gi <peteg0@outlook.com>
2018-09-15 15:10:00 +0200

    Moved and updated translations

--- src/store/lang.js
+++ src/store/lang.js
@@ -1,11 +1,80 @@
 const lang = {
     state: {
         pl: {
-            home: 'Główna',
-            about: 'O mnie',
-            skills: 'Umiejętności',
-            portfolio: 'Portfolio',
-            contact: 'Contact'
+            nav: {
+                home: 'Główna',
+                about: 'O mnie',
+                skills: 'Umiejętności',
+                portfolio: 'Portfolio',
+                experience: 'Doświadczenie',
+            },
+            about: [
+                'Cześć, jestem Piotr, web developer i wanna-be frontend developer.',
+                'Można powiedzieć, że wychowałem się na RWD, przez ponad 4 lata pracy nie zrobiłem ani jednej nieresponsywnej strony.',
+                'Jak widać, moja strona jest prosta, szybka i (mam nadzieję) czytelna. Żadnego przerostu formy nad treścią. Lubię minimalizm, proste rozwiązania, funkcjonalność i pierogi ruskie. Jestem fanbojem Vue.',
+                'Tworzeniem stron internetowych zainteresowałem się około roku 2004 - tabele, iframe\'y, te sprawy. Sam się uczyłem, sam zdobywałem wiedzę. W branży pracuję od 2014 roku.',
+                'Jestem na bieżąco z wieloma informacjami ze świata frontendu. Wiem gdy wychodzi nowa wersja Angulara czy Reacta, wiem kiedy popularność zdobywa kolejny framework JS, dowiaduję się o statystykach użytkowania narzędzi developerskich na świecie, czytam o nadchodzących możliwościach HTML/CSS/JS. Lubię być w miarę na bieżąco.',
+                'Programowanie to dla mnie nie tylko praca, ale i hobby i pasja. Poświęcam temu masę czasu.',
+                'Chcę tworzyć duże rzeczy i skupiać się na frontendzie. Staram się unikać jQuery mimo tego wszystkiego co zrobiło dla dzisiejszego webdevu (thx jquery).'
+            ],
+            portfolioCard: [
+                'HTML/CSS/JS',
+                'Vue.js',
+                'Concrete5 CMS',
+                'PHP',
+                'Laravel'
+            ],
+            portfolio: [
+                {
+                    img: 'lapampa.pl',
+                    name: 'La Pampa',
+                    desc: 'Strona internetowa dla najbardziej soczystej restauracji w Gdańsku'
+                }, {
+                    img: 'biurowiec-koszalin.pl',
+                    name: 'Biurowiec Drewexim',
+                    desc: 'Wynajem lokali usługowych w Koszalinie'
+                }, {
+                    img: 'doplaty-do-odszkodowan.pl',
+                    name: 'Dopłaty do odszkodowań',
+                    desc: 'Firma pomocna w uzyskiwaniu odszkodowań za wypadki samochodowe'
+                }, {
+                    img: 'lagunasmaku.pl',
+                    name: 'Laguna Smaku',
+                    desc: 'Restauracja w Sopocie, położona przy samym molo'
+                }, {
+                    img: 'quiza.pl',
+                    name: 'Quiza',
+                    desc: 'Najsłodszy importer bananów w Polsce'
+                }, {
+                    img: 'wrozbita-maciej.pl',
+                    name: 'Wróżbita Maciej Skrzątek',
+                    desc: 'Któż nie zna najsławniejszego kłam... wróżbity w kraju?'
+                }, {
+                    img: 'internet-media.pl',
+                    name: 'Internet Media Polska',
+                    desc: 'Strony internetowe na całą Polskę'
+                }
+            ],
+            experience: {
+                over4years: ['Ponad 4', 'lata doświadczenia'],
+                over800websites: ['Ponad 800', 'stron internetowych'],
+                oneDev: ['Tylko jeden', 'web developer'],
+                instructor: 'Nauczyciel',
+                tu: 'Politechnika Koszalińska',
+                inz: 'Studia inżynierskie',
+                im: [
+                    'Tworzenie stron internetowych w oparciu o projekt graficzny',
+                    'Zarządzanie i instruktaże dotyczące obsługi CMS Concrete i Wordpress',
+                    'Tworzenie i rozwój oprogramowania firmowego'
+                ],
+                sda: [
+                    'Przeprowadzenie bloku zajęć w szkole programowania, temat <i>"Narzędzia programistyczne"</i>:<ul><li>NPM</li><li>Webpack</li><li>Grunt</li></ul>'
+                ],
+                pk: [
+                    'Ukończenie kierunku <i>"Edukacja techniczno-informatyczna"</i> ze specjalnością <i>"Technologie internetowe i mobilne"</i>',
+                    'Zdobycie wiedzy na temat HTML, CSS i JavaScript'
+                ]
+            }
         }
     }
 }
