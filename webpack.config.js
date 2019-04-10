8bcdd30c7316ba6d34ec6f29a548c666ad6b793a
Piotr <pgolczyk@blastlab.co>
2019-04-10 08:41:43 +0200

    Change vision

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const src = 'src';
+const dist = 'assets';
+
+const config = {
+    entry: {
+        index: `./${src}/index.js`
+    },
+    output: {
+        path: path.resolve(__dirname, dist),
+        filename: 'js/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|sass|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            processCssUrls: false
+                        }
+                    }
+                ]
+            }, {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css'
+        }),
+        new UglifyJSPlugin({
+            uglifyOptions: {
+                mangle: false,
+                sourcemap: false,
+                output: {
+                    comments: false
+                }
+            }
+        })
+    ]
+};
+
+module.exports = config;
---
+++ webpack.dev.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const src = 'src';
+const dist = 'assets';
+
+const config = {
+    entry: {
+        index: `./${src}/index.js`
+    },
+    output: {
+        path: path.resolve(__dirname, dist),
+        filename: 'js/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|sass|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            processCssUrls: false
+                        }
+                    }
+                ]
+            }, {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css'
+        })
+    ]
+};
+
+module.exports = config;
